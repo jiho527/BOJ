a079b9becff1c5b1645b24eb89a89426d82664de
Jiho Lim <very725@naver.com>
2020-06-14 14:25:28 +0900

    200614 study

---
+++ 유형별 문제풀이/10. 동적 프로그래밍 -기초 문제풀이.py
@@ -0,0 +1,93 @@
+# 동적프로그래밍 대표유형 확실히 파악하기
+
+# 1904번 - 01타일 [틀림] 
+# 난이도 하 / 동적 프로그래밍 / 20분
+
+# 내 풀이 [런타임 에러]
+##def solve(n) :
+##    if n == 1 :
+##        return n
+##    if n == 2 :
+##        return n
+##
+##    return solve(n - 1) + solve(n - 2)
+##
+##n = int(input())
+##
+##print(solve(n))
+
+# 내풀이 2 [메모리 초과]
+##n = int(input())
+##
+##arr = [0]
+##arr.append(1)
+##arr.append(2)
+##
+##for i in range(3, n + 1) :
+##    arr.append(arr[i - 1] + arr[i - 2])
+##
+##print(arr[n] % 15746)
+
+
+# 강의
+# 동적 프로그래밍은 한번 구한 값을 나중에 다시 구하지 않는다. (Memoization)
+# 동적 프로그래밍 문제를 풀기 위해서는 점화식(인접한 항들 사이의 관계식)을 세워야함
+
+# 길이가 i 인 수열을 형성하는 방법
+# [i-1] + 1 / [i - 2] + 00
+# 따라서 D[i] = 수열의 길이가 i일때의 경우의 수라고 하면,
+# D[i] = D[i-1] + D[i-2]
+
+# 따라서 이는 피보나치수열과 동일
+
+##n = int(input())
+##dp = [0] * 1000001
+##dp[1] = 1
+##dp[2] = 2
+##
+##for i in range(3, n + 1) :
+##    dp[i] = (dp[i-2] + dp[i-1]) %15746 # 엄청 큰 값에 대해 대비
+##
+##print(dp[n])
+
+# 동적프로그래밍은 점화식을 잘 세우고 문제가 원하는 게 무엇인지 파악하는 지가 중요!
+
+
+
+# 12865번 - 평범한 배낭 [틀림]
+# 난이도 하/ 동적 프로그래밍 / 30분
+# 가장 많이 나오는 예제 !! 꼭 기억 !! 반복 !!
+
+# 강의
+# 핵심 아이디어 : 모든 무게에 대하여 최대 가치를 저장
+# 시간복잡도 O(NK) <- 행이 N개 열이 K개인 테이블
+
+##n, k = map(int, input().split())
+##
+##dp = [[0] * (k + 1) for _ in range(n + 1)]
+##
+##for i in range(1, n + 1) :
+##    weight, value = map(int, input().split())
+##    for j in range(1, k + 1) :
+##        if j < weight :
+##            dp[i][j] = dp[i - 1][j]
+##        else :
+##            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weight] + value)
+##
+##print(dp[n][k])
+
+# 한번 더
+
+n, k = map(int, input().split())
+
+dp = [[0] * (k+1) for _ in range(n + 1)]
+
+for i in range(1, n + 1) :
+    weight, value = map(int, input().split())
+    for j in range(1, k + 1) :
+        if j < weight :
+            dp[i][j] = dp[i-1][j]
+        else :
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight] + value)
+
+print(dp[n][k])
